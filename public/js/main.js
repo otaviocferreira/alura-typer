97bce2c46ac11ec19bca5470dbffc4659fde0590
Otavio Ferreira <otavio.cferreira@gmail.com>
2018-09-29 15:34:46 -0300

    Capitulo 2 - Aula final

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 var frase = $(".frase").text();
 var qtdPalavras = $("#qtd-palavras");
-
 var palavras = frase.trim().split(" ");
-
-console.log(palavras);
-
-
 qtdPalavras.text(palavras.length);
 
+var campoDigitacao = $(".campo-digitacao");
+campoDigitacao.on("input", function() {
+    var conteudo = campoDigitacao.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    var qtdCaracteres = conteudo.length;
+
+    $("#contador-palavras").text(qtdPalavras);
+    $("#contador-caracteres").text(qtdCaracteres);
+});
